4d27e8d851e8830061bedcc83de335b5b6f9d3b1
KhaleedIBG <167705060+KhaleedIBG@users.noreply.github.com>
2024-04-21 21:41:01 +0100

    Coding on python
    Second Assignment on Python

---
+++ app.py
@@ -0,0 +1,21 @@
+#it will print Welcome to python
+print ("WelCome to Python")
+Name = input ("Whats your Name?")
+# it will print what
+Address = input ("what is your Address?")
+#it will print what is your date of birth
+Date = input ("Please enter your Date of birth in the form of DD/MM/YYYY")
+#it will print which state are you
+State = input ("which State are you?")
+#it will print enter your school Name
+School = input ("Enter Yuor School Name")
+#it will print good morning 
+print ("good morning", Name)
+#it will print  Address
+print ("your Address is", Address)
+#it will print Date
+print ("your Date of Brith is", Date)
+#it will print Staet
+print ("your State is", State)
+#it will print School 
+print ("your School name is", School)
